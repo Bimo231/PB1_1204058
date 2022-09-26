0dc058e2c54be8559e9754358be26f4f68a12735
Bimo Arga Dewantoro <Bimo Arga Dewantoro@IWANDSB-LAPTOP>
2022-09-26 16:27:55 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int angka1, angka2, hasil = 0;
+
+			Console.Write("angka1 = ");
+			angka1 = int.Parse(Console.ReadLine());
+			Console.Write("angka2 = ");
+			angka2 = int.Parse(Console.ReadLine());
+
+			
+
+			Console.WriteLine($"Hasil angka1 + appel = {hasil}");
+			hasil = angka1 + angka2;
+			Console.WriteLine($"Hasil angka1 * appel = {hasil}");
+			hasil = angka1 * angka2;
+
+			
+
+			//Console.Write("angka1 = ");
+			//angka1 = int.Parse(Console.ReadLine());
+			//Console.Write("appel = ");
+			//angka2 = int.Parse(Console.ReadLine());
+
+
+
+
+		}
+	}
+}
